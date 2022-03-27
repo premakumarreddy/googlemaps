c846205dc05b37e342b16636b4d354c9cd93dec6
Premakumar <premakumar.tr@gmail.com>
2022-03-27 21:59:42 +0530

    Add files via upload

---
+++ SimpleLambda.java
@@ -0,0 +1,24 @@
+package de.rieckpil.blog;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
+public class SimpleLambda {
+
+//implements RequestHandler<String, String>
+//	@Override
+//	public String handleRequest(String input, Context context) {
+//		return input.toString().concat("pratap accounter");
+//	}
+
+	public static void main(String[] args) {
+		Hashtable h=new Hashtable();
+		h.put(null, 8);//		forTest();
+	}
+
+//	static void forTest() {
+//		List<String> messages = Arrays.asList("First", "Second", "Third");
+//		messages.forEach(System.out::println);
+//	}
+}
